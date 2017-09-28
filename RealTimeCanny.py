516fed0f0bf3b8fbd92f89ae9ac31c82a70974e3
Albert H Mendrofa <albertmendrofa@gmail.com>
2017-09-28 16:55:27 +0700

    Create RealTimeCanny.py

---
+++ RealTimeCanny.py
@@ -0,0 +1,64 @@
+#Real time canny edge
+
+# OpenCV program to perform Edge detection in real time
+# import libraries of python OpenCV 
+# where its functionality resides
+import cv2 
+ 
+# np is an alias pointing to numpy library
+import numpy as np
+import time
+ 
+ 
+# capture frames from a camera
+cap = cv2.VideoCapture(0) #0 untuk parameter kamera. 0 kamera1
+ 
+ 
+# loop runs if capturing has been initialized
+while(1):
+ 
+    # reads frames from a camera
+    ret, frame = cap.read()
+ 
+    # converting BGR to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+    #Adrian Rosebrock Style
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    style = cv2.Canny(blurred, 50, 200, 255)
+     
+    # define range of red color in HSV
+    lower_red = np.array([30,150,50])
+    upper_red = np.array([255,255,180])
+     
+    # create a red HSV colour boundary and 
+    # threshold HSV image
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+ 
+    # Bitwise-AND mask and original image
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+ 
+    # Display an original image
+    cv2.imshow('Original',frame)
+ 
+    # finds edges in the input image image and
+    # marks them in the output map edges
+    edges = cv2.Canny(frame,100,200)
+ 
+    # Display edges in a frame
+    cv2.imshow('Edges',edges)
+    
+    cv2.imshow('Adrian Edge',style)
+    time.sleep(0.1) 
+    # Wait for Esc key to stop
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+ 
+ 
+# Close the window
+cap.release()
+ 
+# De-allocate any associated memory usage
+cv2.destroyAllWindows() 
